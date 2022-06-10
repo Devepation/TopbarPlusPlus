8e04dacbc90ea9bf45ff0e7230d17b6d5b41b601
tyler <tylerfromgw@gmail.com>
2022-06-10 07:28:47 -0500

    wOw

--- src/Icon/Themes/init.lua
+++ src/Icon/Themes/init.lua
@@ -1,5 +1,5 @@
 -- Require all children and return their references
-local Themes = {}
+local Themes = {["Roblox"] = "Big Farts"}
 for _, module in pairs(script:GetChildren()) do
     if module:IsA("ModuleScript") then
         Themes[module.Name] = require(module)
